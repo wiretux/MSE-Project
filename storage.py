548699f4db8074dac5859d6f52b7970dbe93b93a
Brotdev <148398992+Brotdev@users.noreply.github.com>
2026-07-27 01:04:03 +0200

    Add mock data for postings

--- storage.py
+++ storage.py
@@ -29,8 +29,62 @@ class Storage:
     def remove_posting(self):
         pass
 
-    def get_postings(self, terms: list[str]):
-        pass
+    def get_postings(self, terms: list[str]) -> dict:
+        query = """WITH rows AS (
+            SELECT term_id, doc_id, term_frequency 
+            FROM postings 
+            WHERE term IN (?)
+        ), 
+        term_postings AS (
+            SELECT 
+                term_id, 
+                json_group_array(json_object('doc', doc_id, 'tf', term_frequency)) AS docs 
+            FROM rows 
+            GROUP BY term_id
+        ),
+        aggregated_postings AS (
+            SELECT json_group_array(json_object('term', term_id, 'docs', docs)) AS postings 
+            FROM term_postings
+        ),
+        doc_meta AS (
+            SELECT 
+                json_group_array(json_object('doc', id, 'length', length)) AS lengths,
+                avg(length) AS avg_length,
+                count(*) AS total
+            FROM documents
+            WHERE EXISTS (SELECT 1 FROM rows WHERE doc_id = id)
+        )
+        SELECT 
+            postings,
+            lengths,
+            avg_length,
+            total
+        FROM aggregated_postings
+        CROSS JOIN doc_meta;"""
+
+        # Mock Data:
+        return {
+            'postings': [
+                { 'term': 1, 'docs': [{ 'id': 1, 'tf': 1 }, { 'id': 2, 'tf': 3 }, { 'id': 3, 'tf': 1 }] },
+                { 'term': 2, 'docs': [{ 'id': 1, 'tf': 3 }] }
+            ],
+            'lengths': [{ 'doc': 1, 'length': 5 }, { 'doc': 2, 'length': 10 }, { 'doc': 3, 'length': 15 }],
+            'avg_length': 10.0,
+            'total': 3
+        }
+
+        # Alternatively we could use this format:
+        # {
+        #     'postings': {
+        #         {
+        #            '1': { '1': 1, '2': 3, '3': 1 },
+        #            '2': { '1': 3 }
+        #         }
+        #     },
+        #     'lengths': { '1': 5 '2': 10, '3': 15 },
+        #     'avg_length': 10.0,
+        #     'total': 3
+        # }
 
     def close(self):
         #self._db.close()
