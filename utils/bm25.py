7c384c55d2b5347edbe88df16d7d74704db612ec
Samoojo <105240910+Samoojo@users.noreply.github.com>
2026-07-31 16:40:57 +0200

    apply review suggestion to bm25.py

---
+++ utils/bm25.py
@@ -0,0 +1,47 @@
+import storage
+from text_preprocessor import preprocess_text
+
+LIMIT = 10
+
+
+def get_bm25(query: str, limit: int = 10, k1: float = 1.5, b: float = 0.75):
+    # Preprocess the text to tokens
+    query_tokens = preprocess_text(query)
+
+    with storage.access() as store:
+        # Get the bm25 relevant data from the db
+        postings, term_meta, avg_length, total_docs = store.get_postings(
+            query_tokens, max=200
+        )
+
+        # If there is no average length no ranking is possible
+        if not avg_length:
+            return []
+
+        results = []
+
+        # Loop over every relevant document
+        for doc_id, doc_data in postings.items():
+            # Extract the length and terms from the document
+            doc_length = doc_data["length"]
+            doc_terms = doc_data["terms"]
+
+            # Pre calculate the length normalization
+            length_norm = 1 - b + b * (doc_length / avg_length)
+
+            score = 0
+            # Loop over every term to calculate the bm25 score for the document
+            for term_id, tf in doc_terms.items():
+                idf = term_meta.get(term_id, 0.0)
+                numerator = tf * (k1 + 1)
+                denominator = tf + k1 * length_norm
+                score += idf * numerator / denominator
+
+            # Store the doc_id its score in the result list
+            results.append((doc_id, score))
+
+        # Sort the results
+        ranked_docs = sorted(results, key=lambda doc: doc[1], reverse=True)
+
+        # Return the sorted documents
+        return [doc_id for doc_id, score in ranked_docs[:limit]]
